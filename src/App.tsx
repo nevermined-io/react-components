9f6f624fba9b1bd0ea568f0f569d70b2d8d6bc18
Siamend Darwesh <simo@undev.de>
2021-11-24 18:34:01 +0100

    Update App.tsx, comment out API testing

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import './scss/style.scss';
 import AssetRegistration from './components/AssetRegistration';
 import mapFormDataToMetaData, { MetaDataFormDTO } from './utils/mapFormDataToMetaData';
+import { postMetaData } from './hooks/publishMetaData';
+import { MetaData } from '@nevermined-io/nevermined-sdk-js';
 
 function App() {
-  const onSubmit = (data: MetaDataFormDTO) => {
+  const onSubmit = async (data: MetaDataFormDTO) => {
     console.log('onSubmityes', data);
     const dataToSend = mapFormDataToMetaData('customData', data);
     console.log('mappedData', dataToSend);
+
+    try {
+      //   const res = await postMetaData(dataToSend);
+      //   console.log(res);
+    } catch (e) {
+      console.error(e);
+    }
   };
   return (
     <div className="App">
