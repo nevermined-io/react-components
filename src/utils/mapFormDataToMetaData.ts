9d7c9a03dbd899dd62fb94e89627fab7fb0cd039
Siamend Darwesh <simo@undev.de>
2021-11-24 18:25:12 +0100

    Update MetaData mapping

--- src/utils/mapFormDataToMetaData.ts
+++ src/utils/mapFormDataToMetaData.ts
@@ -1,4 +1,4 @@
-import { MetaData } from '@nevermined-io/nevermined-sdk-js';
+import { File, MetaData } from "@nevermined-io/nevermined-sdk-js";
 
 // export interface FormData {
 //     name: string;
@@ -37,47 +37,48 @@ export interface MetaDataFormDTO {
     copyrightHolder?: string;
 }
 
-type MData = {
-    name: string;
-    type: 'dataset' | 'algorithm' | 'compute' | 'workflow' | 'compute';
-    dateCreated: string;
-    datePublished?: string;
-    author: string;
-    license: string;
-    price: string;
-    files?: File[];
-    encryptedService?: any;
-    workflow?: any;
-    algorithm?: any;
-    service?: any;
-};
+// type MData = {
+//     name: string;
+//     type: 'dataset' | 'algorithm' | 'compute' | 'workflow' | 'compute';
+//     dateCreated: string;
+//     datePublished?: string;
+//     author: string;
+//     license: string;
+//     price: string;
+//     files?: File[];
+//     encryptedService?: any;
+//     workflow?: any;
+//     algorithm?: any;
+//     service?: any;
+// };
+
 /**
  *
  * @param customDataName
  * @param formData
  * @returns mappedFormData matching the Nevermined API
  */
-const mapFormDataToMetaData = (customDataName = "customData", formData: MetaDataFormDTO) => {
-    console.log(formData);
+const mapFormDataToMetaData = (customDataName = "customData", formData: MetaDataFormDTO): MetaData => {
     const { name, author, license, price, files, description, copyrightHolder, ...rest } = formData;
+    console.log("rest", rest);
     return {
         main: {
             type: 'dataset',
-            name: name,
-            dateCreated: (new Date()).toISOString(),
-            datePublished: '',
-            author: author,
-            license: license,
-            price: price,
-            files: files
+            name: name || "",
+            datePublished: `${new Date().toISOString().split('.')[0]}Z`,
+            dateCreated: `${new Date().toISOString().split('.')[0]}Z`, // remove milliseconds
+            author: author || "",
+            license: license || "",
+            price: price || "0",
+            files: files || []
         },
         additionalInformation: {
-            description: description,
-            copyrightHolder: copyrightHolder,
+            description: description || "",
+            copyrightHolder: copyrightHolder || "",
             categories: [],
             links: [],
             tags: [],
-            updateFrequency: null,
+            updateFrequency: undefined,
             structuredMarkup: []
         },
         [customDataName]: rest
