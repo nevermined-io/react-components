bcd8e46d2097999ac4b66f77afbea0bda09daba4
Siamend Darwesh <simo@undev.de>
2021-12-08 19:45:09 +0100

    Formatting

--- src/lib/components/AssetRegistration/FormField.test.tsx
+++ src/lib/components/AssetRegistration/FormField.test.tsx
@@ -1,23 +1,18 @@
-import React from "react";
-import FormField from "./FormField";
-import { render } from '@testing-library/react'
-import MetaDataFormProvider from "lib/contexts/forms/MetaDataFormProvider";
+import React from 'react';
+import FormField from './FormField';
+import { render } from '@testing-library/react';
+import MetaDataFormProvider from 'lib/contexts/forms/MetaDataFormProvider';
 
-describe("FormField", () => {
-  it("renders without crashing", () => {
+describe('FormField', () => {
+  it('renders without crashing', () => {
     const { container } = render(
       <MetaDataFormProvider>
-      <FormField
-        label="Yo"
-        type="textarea"
-        className="textarea"
-        id="test"
-      />
+        <FormField label="Yo" type="textarea" className="textarea" id="test" />
       </MetaDataFormProvider>
     );
 
-    expect(container.querySelector("textarea")).toBeDefined();
-    expect(container.querySelector("label")).toHaveClass("textarea-label");
-
-  })
+    expect(container.querySelector('textarea')).toBeDefined();
+    expect(container.querySelector('label')).toHaveClass('textarea-label');
+  });
 });
+s
