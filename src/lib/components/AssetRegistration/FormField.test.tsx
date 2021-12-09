6eb19492db6d90f4a1a018ed1bc3b12b79b630c0
Siamend Darwesh <simo@undev.de>
2021-12-09 12:15:22 +0100

    Update FormField test

--- src/lib/components/AssetRegistration/FormField.test.tsx
+++ src/lib/components/AssetRegistration/FormField.test.tsx
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import FormField from './FormField';
 import { render } from '@testing-library/react';
 import MetaDataFormProvider from 'lib/contexts/forms/MetaDataFormProvider';
 
+const renderWithProvider = (comp: ReactNode) => {
+  return render(<MetaDataFormProvider>{comp}</MetaDataFormProvider>);
+};
+
 describe('FormField', () => {
-  it('renders without crashing', () => {
-    const { container } = render(
-      <MetaDataFormProvider>
-        <FormField label="Yo" type="textarea" className="textarea" id="test" />
-      </MetaDataFormProvider>
+  it('renders textarea', () => {
+    const { container } = renderWithProvider(
+      <FormField label="Yo" type="textarea" className="test-class" id="test" />
     );
 
-    expect(container.querySelector('textarea')).toBeDefined();
-    expect(container.querySelector('label')).toHaveClass('textarea-label');
+    expect(container.querySelector('textarea')).toBeTruthy();
+    expect(container.querySelector('label')).toHaveClass('test-class-label');
+  });
+
+  it('renders text input', () => {
+    const { container } = renderWithProvider(
+      <FormField label="Yo" type="text" className="test-class" id="test" />
+    );
+
+    expect(container.querySelector('input[type="text"]')).toBeTruthy();
+    expect(container.querySelector('label')).toHaveClass('test-class-label');
+  });
+
+  it('renders number input', () => {
+    const { container } = renderWithProvider(
+      <FormField label="Yo" type="number" className="test-class" id="test" />
+    );
+
+    expect(container.querySelector('input[type="number"]')).toBeTruthy();
+    expect(container.querySelector('input[type="number"]')).toHaveClass('test-class-number');
+    expect(container.querySelector('label')).toHaveClass('test-class-label');
+  });
+
+  it('renders checkbox input', () => {
+    const { container } = renderWithProvider(
+      <FormField label="Yo" type="checkbox" className="test-class" id="test" />
+    );
+
+    expect(container.querySelector('input[type="checkbox"]')).toBeTruthy();
+    expect(container.querySelector('label')).toHaveClass('test-class-label');
   });
 });
