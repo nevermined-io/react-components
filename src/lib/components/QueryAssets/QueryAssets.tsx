7062b0fa63885bb6d14805a2bfd05bb1a37037d0
Pedro Gutiérrez <pedrogp9693@gmail.com>
2021-12-10 14:54:12 +0100

    Request initial data

--- src/lib/components/QueryAssets/QueryAssets.tsx
+++ src/lib/components/QueryAssets/QueryAssets.tsx
@@ -1,11 +1,41 @@
 import React, { useEffect, useState } from 'react';
+import { DDO } from '@nevermined-io/nevermined-sdk-js';
+
 import { useNevermined } from 'lib/contexts/NeverminedProvider';
 
 interface QueryAssetsProps {
+  infinite?: boolean // works with pagination or infinite
+}
+
+interface QueryStats {
+  page: number
+  totalPages: number
+  totalResults: number
 }
 
 export const NuiQueryAssets = React.memo(function ({}: QueryAssetsProps) {
   const { sdk } = useNevermined();
+  const [ assets, setAssets ] = useState<DDO[]>([]);
+  const [ stats, setStats ] = useState<QueryStats>();
+
+  useEffect(() => {
+    if (!sdk?.assets) {
+      return
+    }
+    sdk.assets
+      .query({
+        offset: 10,
+        page: 1,
+        query: {},
+        sort: {
+          created: -1
+        },
+      })
+      .then(result => {
+        setStats({...result, results: undefined} as any)
+        setAssets(result.results)
+      })
+  }, [sdk])
 
   return <>QueryAssets</>
 });
