6c15caf196d24a1fc267af2c370ab76264ad93ca
Siamend Darwesh <simo@undev.de>
2021-12-23 11:56:03 +0100

    Styling of example, restructure so you can view single components

--- src/Example.tsx
+++ src/Example.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './scss/style.scss';
 import mapFormDataToMetaData from 'lib/utils/mapFormDataToMetaData';
 
@@ -20,7 +20,7 @@ import { KeykoLogo } from './lib/components/Layout/Logos/KeykoLogo';
 
 function Example() {
   const { registerAsset, retrieveAssetDDO } = useAssetRegistration();
-
+  const [currentPage, setCurrentPage] = useState(0);
   const onSubmit = async (data: MetaDataFormDTO) => {
     const dataToSend = mapFormDataToMetaData('jochenname', data);
     console.error('mappedData', dataToSend);
@@ -73,48 +73,64 @@ function Example() {
       </Header>
       <Main>
         <article>
-          <section>
-            <NeverminedLogo width={256} height={256} />
-          </section>
-          <section>
-            <AssetRegistration
-              onSubmit={onSubmit}
-              onSubmitError={onSubmitError}
-              detailFields={[
-                { id: 'name', label: 'Asset Name', type: 'text' },
-                { id: 'description', label: 'Asset Description:', type: 'textarea' },
-                { id: 'testing', label: 'One thing:', type: 'textarea' },
-                { id: 'something', label: 'Something:', type: 'textarea' }
-              ]}
-              authorshipFields={[
-                { id: 'onething', label: 'One thing:', type: 'textarea' },
-                { id: 'someimage', label: 'Some image:', type: 'file', mimeType: 'image/*' }
-              ]}
-              pricingFields={[{ id: 'anotherthing', label: 'Another thing:', type: 'textarea' }]}
-            />
-            {/* {!isLoggedIn && <div>not logged in</div>} */}
-            {/* <button onClick={handleSubmit(onSubmit, onSubmitError)} type="button">
+          <nav>
+            <ul>
+              <li onClick={() => setCurrentPage(0)}>Asset Registration</li>
+              <li onClick={() => setCurrentPage(1)}>Query Assets</li>
+              <li onClick={() => setCurrentPage(2)}>Token Components</li>
+            </ul>
+          </nav>
+          {currentPage === 0 && (
+            <section>
+              <AssetRegistration
+                onSubmit={onSubmit}
+                onSubmitError={onSubmitError}
+                detailFields={[
+                  { id: 'name', label: 'Asset Name', type: 'text' },
+                  { id: 'description', label: 'Asset Description:', type: 'textarea' },
+                  { id: 'testing', label: 'One thing:', type: 'textarea' },
+                  { id: 'something', label: 'Something:', type: 'textarea' }
+                ]}
+                authorshipFields={[
+                  { id: 'onething', label: 'One thing:', type: 'textarea' },
+                  { id: 'someimage', label: 'Some image:', type: 'file', mimeType: 'image/*' }
+                ]}
+                pricingFields={[{ id: 'anotherthing', label: 'Another thing:', type: 'textarea' }]}
+              />
+              {/* {!isLoggedIn && <div>not logged in</div>} */}
+              {/* <button onClick={handleSubmit(onSubmit, onSubmitError)} type="button">
         Submit
       </button> */}
-          </section>
-          <section>
-            <NuiQueryAssets>
-              {(assets, info, goNext, goPrev) => {
-                return (
-                  <>
-                    {assets.map((_) => _.id).join(', ')}
-                    <br />
-                    <br />
-                    {JSON.stringify(info, null, 2)}
-                    <br />
-                    <br />
-                    {info.canGoPrev && <span onClick={goPrev}> Prev </span>}
-                    {info.canGoNext && <span onClick={goNext}> Next </span>}
-                  </>
-                );
-              }}
-            </NuiQueryAssets>
-          </section>
+            </section>
+          )}
+          {currentPage === 1 && (
+            <section>
+              <NuiQueryAssets>
+                {(assets, info, goNext, goPrev) => {
+                  return (
+                    <>
+                      {assets.map((_) => _.id).join(', ')}
+                      <br />
+                      <br />
+                      {JSON.stringify(info, null, 2)}
+                      <br />
+                      <br />
+                      {info.canGoPrev && <span onClick={goPrev}> Prev </span>}
+                      {info.canGoNext && <span onClick={goNext}> Next </span>}
+                    </>
+                  );
+                }}
+              </NuiQueryAssets>
+            </section>
+          )}
+          {currentPage === 2 && (
+            <section>
+              <NuiTokenPrice address="0x022E292b44B5a146F2e8ee36Ff44D3dd863C915c">
+                1234567890000000000
+              </NuiTokenPrice>{' '}
+              <NuiTokenName address="0x022E292b44B5a146F2e8ee36Ff44D3dd863C915c" />
+            </section>
+          )}
         </article>
       </Main>
       <Footer>
