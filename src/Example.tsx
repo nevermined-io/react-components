1bbb5fc99d96561621298151f80e1d126543326f
Siamend Darwesh <simo@undev.de>
2021-12-23 13:03:20 +0100

    Refactor examples

--- src/Example.tsx
+++ src/Example.tsx
@@ -17,6 +17,9 @@ import Main from 'lib/components/Layout/Main';
 
 import { NeverminedLogo } from './lib/components/Layout/Logos/Logo';
 import { KeykoLogo } from './lib/components/Layout/Logos/KeykoLogo';
+import QueryAssetExample from './examples/QueryAssetExample';
+import AssetRegistrationExample from './examples/AssetRegistrationExample';
+import TokenExample from './examples/TokenExample';
 
 function Example() {
   const { registerAsset, retrieveAssetDDO } = useAssetRegistration();
@@ -81,88 +84,10 @@ function Example() {
             </ul>
           </nav>
           {currentPage === 0 && (
-            <section>
-              <AssetRegistration
-                onSubmit={onSubmit}
-                onSubmitError={onSubmitError}
-                detailFields={[
-                  { id: 'name', label: 'Asset Name', type: 'text' },
-                  { id: 'description', label: 'Asset Description:', type: 'textarea' },
-                  { id: 'testing', label: 'One thing:', type: 'textarea' },
-                  { id: 'something', label: 'Something:', type: 'textarea' }
-                ]}
-                authorshipFields={[
-                  { id: 'onething', label: 'One thing:', type: 'textarea' },
-                  { id: 'someimage', label: 'Some image:', type: 'file', mimeType: 'image/*' }
-                ]}
-                pricingFields={[{ id: 'anotherthing', label: 'Another thing:', type: 'textarea' }]}
-              />
-              {/* {!isLoggedIn && <div>not logged in</div>} */}
-              {/* <button onClick={handleSubmit(onSubmit, onSubmitError)} type="button">
-        Submit
-      </button> */}
-            </section>
+            <AssetRegistrationExample onSubmit={onSubmit} onSubmitError={onSubmitError} />
           )}
-          {currentPage === 1 && (
-            <section className="query-assets">
-              <NuiQueryAssets>
-                {(assets, info, goNext, goPrev) => {
-                  return (
-                    <>
-                      {JSON.stringify(info, null, 2)}
-                      <ul>
-                        {assets.map((ddo: DDO) => {
-                          const {
-                            id,
-                            service: [metadata, ...rest]
-                          } = ddo;
-                          const {
-                            attributes: {
-                              additionalInformation: { description }
-                            }
-                          } = metadata;
-
-                          return (
-                            <li key={id}>
-                              <ul>
-                                <li>
-                                  <label>ID:</label>
-                                  <span>{id}</span>
-                                </li>
-                                <li>
-                                  <label>Description:</label>
-                                  <span>{description}</span>
-                                </li>
-                              </ul>
-                            </li>
-                          );
-                        })}
-
-                        {info.canGoPrev && (
-                          <li>
-                            <button onClick={goPrev}> Prev </button>
-                          </li>
-                        )}
-                        {info.canGoNext && (
-                          <li>
-                            <button onClick={goNext}> Next </button>
-                          </li>
-                        )}
-                      </ul>
-                    </>
-                  );
-                }}
-              </NuiQueryAssets>
-            </section>
-          )}
-          {currentPage === 2 && (
-            <section>
-              <NuiTokenPrice address="0x022E292b44B5a146F2e8ee36Ff44D3dd863C915c">
-                1234567890000000000
-              </NuiTokenPrice>{' '}
-              <NuiTokenName address="0x022E292b44B5a146F2e8ee36Ff44D3dd863C915c" />
-            </section>
-          )}
+          {currentPage === 1 && <QueryAssetExample />}
+          {currentPage === 2 && <TokenExample />}
         </article>
       </Main>
       <Footer>
---
+++ src/examples/QueryAssetExample.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { NuiQueryAssets } from 'lib/components/QueryAssets';
+
+const QueryAssetExample = () => {
+  return (
+    <section className="query-assets">
+      <NuiQueryAssets>
+        {(assets, info, goNext, goPrev) => {
+          return (
+            <>
+              {JSON.stringify(info, null, 2)}
+              <ul>
+                {assets.map((ddo: DDO) => {
+                  const {
+                    id,
+                    service: [metadata, ...rest]
+                  } = ddo;
+                  const {
+                    attributes: {
+                      additionalInformation: { description }
+                    }
+                  } = metadata;
+
+                  return (
+                    <li key={id}>
+                      <ul>
+                        <li>
+                          <label>ID:</label>
+                          <span>{id}</span>
+                        </li>
+                        <li>
+                          <label>Description:</label>
+                          <span>{description}</span>
+                        </li>
+                      </ul>
+                    </li>
+                  );
+                })}
+
+                {info.canGoPrev && (
+                  <li>
+                    <button onClick={goPrev}> Prev </button>
+                  </li>
+                )}
+                {info.canGoNext && (
+                  <li>
+                    <button onClick={goNext}> Next </button>
+                  </li>
+                )}
+              </ul>
+            </>
+          );
+        }}
+      </NuiQueryAssets>
+    </section>
+  );
+};
+
+export default QueryAssetExample;
