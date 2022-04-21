c2b513cdfa94d37a3d964c0004421e8330e73984
Esraa Jbara <jbesraa@gmail.com>
2022-04-21 10:41:44 +0300

    rm current assets query

--- src/examples/QueryAssetExample.tsx
+++ src/examples/QueryAssetExample.tsx
@@ -1,14 +1,55 @@
-import React from 'react';
-import { NuiQueryAssets } from './components/QueryAssets';
+import React, { useEffect, useState } from 'react';
+import { DDO } from '@nevermined-io/nevermined-sdk-js';
+import { useNevermined } from 'lib/contexts/NeverminedProvider';
+
+export interface QueryAssetsProps {
+  infinite?: boolean;
+  size?: number;
+  children: (
+    assets: DDO[],
+    info: QueryStats & { canGoNext: boolean; canGoPrev: boolean },
+    goNext: () => void,
+    goPrev?: () => void
+  ) => any;
+}
+
+export interface QueryStats {
+  page: number;
+  totalPages: number;
+  totalResults: number;
+}
 
 const QueryAssetExample = () => {
+  const { sdk } = useNevermined();
+  const [assets, setAssets] = useState<DDO[]>([]);
+
+  useEffect(() => {
+    if (!sdk?.assets) {
+      return;
+    }
+    fetchAllAssets();
+  }, [sdk]);
+
+  const fetchAllAssets = () => {
+    sdk?.assets
+      .query({
+        offset: 100,
+        page: 1,
+        query: {},
+        sort: {
+          created: -1
+        }
+      })
+      .then((result) => {
+        console.log('result', result);
+        //       setStats({ ...result, results: undefined } as any);
+        setAssets(result.results);
+      });
+  };
+
   return (
     <div>
-      <NuiQueryAssets>
-        {(assets, info, goNext, goPrev) => {
-          return <div style={{ fontSize: '28px' }}>{JSON.stringify(info, null, 6)}</div>;
-        }}
-      </NuiQueryAssets>
+      <div style={{ fontSize: '28px' }}>{JSON.stringify(assets, null, 6)}</div>
     </div>
   );
 };
