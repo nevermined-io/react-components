7035f925e0b0b0d5cd29dc7da5352790aad39dc3
Esraa Jbara <jbesraa@gmail.com>
2022-05-16 11:53:22 +0300

    update example

--- example/src/examples/index.tsx
+++ example/src/examples/index.tsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Catalog from 'hello-catalog';
+import { DDO } from '@nevermined-io/nevermined-sdk-js';
 
-const Example = (props: any) => {
-  const ethProvider = Catalog.getEtheruemProvider();
-  const { isAvailable: isEthAvailable } = Catalog.useWeb3Service(ethProvider);
+export const allAssetsDefaultQuery = {
+  offset: 2, // limit response to 2 items
+  page: 1,
+  query: {},
+  sort: {
+    created: -1
+  }
+};
+
+const App = (props: any) => {
+  const { sdk } = Catalog.useNevermined();
+  const { useFetchAssets, assets, isLoadingFetchAssets } = Catalog.useAssetService();
+  useFetchAssets(allAssetsDefaultQuery);
 
   return (
     <>
-      <div>Is Etheruem available: </div>
-      <div>{String(isEthAvailable())}</div>
+      <div>Is SDK Avaialable:</div>
+      <div>{Object.keys(sdk).length > 0 ? 'Yes' : 'No'}</div>
+      <div>Is Loading Assets</div>
+      <div>{isLoadingFetchAssets ? 'Yes' : 'No'}</div>
+      <div>Assets:</div>
+      <div>
+        {assets?.map((asset: DDO) => (
+          <div key={asset.id}>{asset.id}</div>
+        ))}
+      </div>
     </>
   );
 };
 
-export default Example;
+export default App;
