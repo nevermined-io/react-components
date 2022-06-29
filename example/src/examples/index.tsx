d5a7995ce08c8b84e01fd4a1a672292f3c5b478a
Esraa Jbara <jbesraa@gmail.com>
2022-06-29 10:32:46 +0300

    components to work with catalog

--- example/src/examples/index.tsx
+++ example/src/examples/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import Catalog from '@nevermined-io/components-catalog';
-import { CollectionItem } from '@nevermined-io/components-catalog/dist/node/types';
+import { AssetState, MintNFTInput } from '@nevermined-io/components-catalog/dist/node/types';
 
-const App = (props: any) => {
+const SDKInstance = () => {
   const { sdk, isLoadingSDK } = Catalog.useNevermined();
-  const { isLoading: isLoadingAssets, allArtwork } = Catalog.useAllAssets();
 
   return (
     <>
@@ -12,14 +11,126 @@ const App = (props: any) => {
       <div>{isLoadingSDK ? 'Yes' : 'No'}</div>
       <div>Is SDK Avaialable:</div>
       <div>{sdk && Object.keys(sdk).length > 0 ? 'Yes' : 'No'}</div>
+    </>
+  );
+};
+
+const SingleAsset = () => {
+  const did = 'did:nv:f8a00c4881721f3c1c1762c280c665ee85f12265798075198129667626fad907';
+  const assetData: AssetState = Catalog.useAsset(did);
+
+  return (
+    <>
+      <div>Asset {did.slice(0, 10)}...:</div>
+      <div>{JSON.stringify(assetData.ddo)}</div>
+    </>
+  );
+};
+
+const MultipleAssets = () => {
+  const { isLoading: isLoadingAssets, result } = Catalog.useAssets(q);
+
+  return (
+    <>
       <div>Is Loading Assets</div>
       <div>{isLoadingAssets ? 'Yes' : 'No'}</div>
       <div>Assets:</div>
-      <div>
-        {allArtwork?.map((asset: CollectionItem) => (
-          <div key={asset.artwork.id}>{asset.artwork.id}</div>
-        ))}
-      </div>
+      <div>{result && JSON.stringify(result?.results)}</div>
+    </>
+  );
+};
+
+export interface MetaDataMain {
+  name: string;
+  type: 'dataset' | 'algorithm' | 'compute' | 'workflow' | 'compute';
+  dateCreated: string;
+  datePublished?: string;
+  author: string;
+  license: string;
+  price: string;
+  //   files?: File[];
+  //   encryptedService?: any;
+  //   workflow?: Workflow;
+  //   algorithm?: Algorithm;
+  //   service?: Service;
+}
+
+export interface MetaData {
+  userId?: string;
+  main: MetaDataMain;
+  //encryptedFiles?: string;
+  //additionalInformation?: AdditionalInformation;
+  //curation?: Curation;
+}
+
+export interface _MintNFTInput {
+  metadata: MetaData;
+  //publisher: Account;
+  cap: number;
+  royalties: number;
+  //assetRewards: AssetRewards;
+  nftAmount?: number;
+  erc20TokenAddress?: string;
+  preMint?: boolean;
+  nftMetadata?: string;
+  //txParams?: TxParameters;
+}
+
+const MintAsset = () => {
+  const { assets, sdk } = Catalog.useNevermined();
+  const metadata: MetaData = {
+    main: {
+      name: '',
+      type: 'dataset',
+      author: '',
+      license: '',
+      dateCreated: '',
+      price: ''
+    }
+  };
+
+  const mint = async () => {
+    try {
+      const [publisher] = await sdk.accounts.list();
+      const data: MintNFTInput = {
+        // metadata,
+        // publisher,
+        // cap,
+        // royalties,
+        // assetRewards
+      } as MintNFTInput;
+      const response = await assets.mint(data);
+      console.log('response', response);
+    } catch (error) {
+      console.log('error', error);
+    }
+  };
+
+  return (
+    <>
+      <button onClick={mint} disabled={!Object.keys(assets).length}>
+        mint
+      </button>
+    </>
+  );
+};
+
+const q = {
+  offset: 150,
+  page: 1,
+  query: {},
+  sort: {
+    created: 'desc'
+  }
+};
+
+const App = (props: any) => {
+  return (
+    <>
+      <SDKInstance />
+      <MintAsset />
+      {/**<MultipleAssets />**/}
+      <SingleAsset />
     </>
   );
 };
