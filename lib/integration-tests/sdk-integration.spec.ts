f3bf53220157d7e776edf3b80cdf3a0f82c8c57f
spieljs <spieljs@gmail.com>
2022-08-26 15:53:18 +0200

    remove logs

--- lib/integration-tests/sdk-integration.spec.ts
+++ lib/integration-tests/sdk-integration.spec.ts
@@ -4,12 +4,6 @@ import { appConfig } from "./config";
 describe('SDK Integration', () => {
 
   it('initialize sdk successfully', async () => {
-    if(process.env.INFURA_TOKEN) {
-      console.log('THERE IS A INFURA TOKEN');
-    }else {
-      console.error('CANNOT FOUND A INFURA TOKEN');
-    }
-
     const response = await initializeNevermined(appConfig);
     expect(response.success).toEqual(true);
   });
