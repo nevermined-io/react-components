1265af2e5d6c18b4fc1cc3cc77fff392d535220e
spieljs <spieljs@gmail.com>
2022-08-26 14:30:37 +0200

    check if infura token exits

--- lib/integration-tests/sdk-integration.spec.ts
+++ lib/integration-tests/sdk-integration.spec.ts
@@ -4,6 +4,12 @@ import { appConfig } from "./config";
 describe('SDK Integration', () => {
 
   it('initialize sdk successfully', async () => {
+    if(process.env.INFURA_TOKEN) {
+      console.log('THERE IS A INFURA TOKEN');
+    }else {
+      console.error('CANNOT FOUND A INFURA TOKEN');
+    }
+
     const response = await initializeNevermined(appConfig);
     expect(response.success).toEqual(true);
   });
