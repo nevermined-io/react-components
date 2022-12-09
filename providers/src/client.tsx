2cdd0b77f437d14379a2a6bc0ab4555c452b81ef
Iurii Tankievych <iurii.tankievych@outlook.com>
2022-12-09 09:42:26 +0000

    Revert "version"
    This reverts commit d001b2c5be331247bc1d7c7e6154390d9d246bc8.

--- providers/src/client.tsx
+++ providers/src/client.tsx
@@ -15,7 +15,7 @@ import { configureChains,
     useDisconnect,
     useSwitchNetwork
 } from "wagmi"
-import { ConnectKitProvider } from 'connectkit/build/index'
+import { ConnectKitProvider } from 'connectkit'
 import { Provider } from "@ethersproject/abstract-provider"
 import { WalletConnectConnector } from "wagmi/connectors/walletConnect"
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
