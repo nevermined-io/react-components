2b677739f26e842d9dedee8e62dea9824d3beb7a
spieljs <spieljs@gmail.com>
2023-01-31 17:56:42 +0100

    update contract version in CI

--- lib/integration-tests/config.ts
+++ lib/integration-tests/config.ts
@@ -6,7 +6,7 @@ import { ethers } from 'ethers'
 const neverminedNodeAddress =
   process.env.NEVERMINED_NODE_ADDRESS || '0x068ed00cf0441e4829d9784fcbe7b9e26d4bd8d0'
 const neverminedNodeUri = process.env.NEVERMINED_NODE_URI || 'http://localhost:8030'
-const marketplaceUri = process.env.MARKETPLACE_URI || 'http://nevermined-metadata:3100'
+const marketplaceUri = process.env.MARKETPLACE_URI || 'http://localhost:3100'
 const web3ProviderUri = process.env.WEB3_PROVIDER_URI || `http://localhost:8545`
 
 Logger.setLevel(3)
